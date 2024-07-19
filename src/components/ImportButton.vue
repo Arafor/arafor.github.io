d505eac6f0119fbd6102266b3972ae8d4e642364
Arafor <zeroartis@hotmail.com>
2024-07-19 17:40:32 +0300

    Import via file

--- src/components/ImportButton.vue
+++ src/components/ImportButton.vue
@@ -7,27 +7,54 @@ const emit = defineEmits(['imported'])
 
 const importModalVisible = ref(false);
 const inputString = ref('')
-const showImportError = ref(false);
+const fileString = ref('')
+const importErrors = ref({
+    string: [] as string[],
+    file: [] as string[],
+});
 
-function handleImport() {
-    showImportError.value = false;
+function handleImport(data: string, type: string) {
+    if (type !== 'string' && type !== 'file') return;
+    importErrors.value[type] = [];
 
     let importedData;
     try {
-        importedData = JSON.parse(inputString.value);
+        console.log(data);
+        importedData = JSON.parse(data);
     } catch {
-        showImportError.value = true;
+        console.log('invalid json');
+        importErrors.value[type].push('Failed to parse json');
         return;
     }
 
     if (!isCharacterSheet(importedData)) {
-        showImportError.value = true;
+        console.log('invalid character sheet');
+        importErrors.value[type].push('Json does not match character sheet structure');
         return;
     }
 
     emit('imported', importedData)
 
     importModalVisible.value = false;
+}
+
+async function handleFileUpload($event: Event) {
+    importErrors.value.file = [];
+
+    const target = $event.target as HTMLInputElement;
+    if (!target || !target.files) {
+        importErrors.value.file.push('Failed to read file');
+        return;
+    }
+
+    const blob = target.files[0];
+    if (blob.type !== 'application/json') {
+        importErrors.value.file.push('File is not json');
+        return;
+    }
+
+    const text = await blob.text();
+    fileString.value = text;
 }
 </script>
 
@@ -40,9 +67,17 @@ function handleImport() {
             </template>
             <template #content>
                 <div class="import-block">
+                    <h4>From json string</h4>
                     <textarea v-model="inputString"></textarea>
-                    <span v-if="showImportError" class="error-message">Failed to import json</span>
-                    <button @click="handleImport">Import</button>
+                    <span v-if="importErrors.string.length" class="error-message">{{ `${importErrors.string.join('\n')}`
+                        }}</span>
+                    <button @click="handleImport(inputString, 'string')">Import</button>
+
+                    <h4>From json file</h4>
+                    <input type="file" accept="application/json" @change="handleFileUpload">
+                    <span v-if="importErrors.file.length" class="error-message">{{ `${importErrors.file.join('\n')}`
+                        }}</span>
+                    <button @click="handleImport(fileString, 'file')">Import</button>
                 </div>
             </template>
         </Modal>
@@ -50,6 +85,10 @@ function handleImport() {
 </template>
 
 <style lang="scss" scoped>
+input[type="file"] {
+    width: 100%;
+}
+
 .import-button {
     display: inline;
 
@@ -65,6 +104,14 @@ function handleImport() {
 
     button {
         margin-top: 16px;
+    }
+
+    h4 {
+        margin-top: 48px;
+
+        &:first-child {
+            margin-top: 0;
+        }
     }
 }
 
